f9ec492fe036282734a659823684de1289ebf441
MarcosKern <vkg.marcos@gmail.com>
2025-04-21 00:49:26 -0300

    Update Header.jsx

--- BroxadaSinistra/src/Components/Header/Header.jsx
+++ BroxadaSinistra/src/Components/Header/Header.jsx
@@ -4,17 +4,6 @@ import './header.css'
 import getTimeRemaining from '../../Services/countdown'
 
 export default function Header() {
-  const [hours, setHours] = useState()
-  const [days, setDays] = useState()
-  const targetDate = new Date('2023-09-01 19:00:00')
-
-  function displayCountdown() {
-    const countdown = getTimeRemaining(targetDate)
-    setHours(countdown.hours)
-    setDays(countdown.days)
-  }
-
-  setInterval(displayCountdown, 1000)
   return (
     <header>
       <img
@@ -29,19 +18,8 @@ export default function Header() {
         id='date'
         className="animate__animated animate__fadeIn animate__delay-1s"
       >
-        DIA 01/09 AS 19 HORAS
+        DIA 01/09/2023 AS 19 HORAS 
       </p>
-      <h2
-        id="countdown"
-        className="animate__animated animate__fadeIn animate__delay-1s"
-      >
-        {
-          days === 0
-            ? 'É HOJE'
-            : `FALTAM ${days} DIA${days != 1 && 'S'} E ${hours} HORA${hours != 1 && 'S'}!`
-        }
-        
-      </h2>
     </header>
   )
 }
